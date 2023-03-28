d2a49ddf7e5338e58cb9e4f0dff4faa353a306e1
Adriana Jimenez <adriana.jmz.prz@gmail.com>
2023-03-27 19:33:09 -0600

    Se agrego los cambios a js

---
+++ JS-04 - Arrays and Loops/js/main.js
@@ -0,0 +1,14 @@
+let fila =["Sofia", "David", "Juan"];
+console.log("Parte #1");
+console.log(fila);
+fila.push("Sara");
+fila.push("Agustin");
+fila.shift();
+console.log("Parte #2");
+console.log(fila)
+fila.splice(1,0, "Renata");
+fila.push("Elena");
+fila.shift();
+console.log("Parte #1");
+
+
