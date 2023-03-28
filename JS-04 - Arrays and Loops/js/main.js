ed1434ecf18b2e654ea7c3c644f53e4e639d8bcd
Adriana Jimenez <adriana.jmz.prz@gmail.com>
2023-03-27 20:08:36 -0600

    Se acompleto el ejercicio 2 completo

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,3 +1,4 @@
+console.log("**Ejercicio 1**")
 let fila =["Sofia", "David", "Juan"];
 console.log("Parte #1");
 console.log(fila);
@@ -11,4 +12,14 @@ fila.push("Elena");
 fila.shift();
 console.log("Parte #1");
 
+console.log("** Ejercicio 2**")
 
+for(let i = 1; i <= 5; ++i ) {
+   let resultado = "";
+
+   for(let j = 1; j <= i; ++j){
+      resultado += "* ";
+   }console.log(resultado);
+}
+
+ 
