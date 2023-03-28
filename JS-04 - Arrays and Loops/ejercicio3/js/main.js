2809e7fe1def06c6318862358e0276f0115bf7f7
Adriana Jimenez <adriana.jmz.prz@gmail.com>
2023-03-27 21:30:51 -0600

    se agrego primera parte del ejercicio 3

---
+++ JS-04 - Arrays and Loops/ejercicio3/js/main.js
@@ -0,0 +1,16 @@
+console.log("***EJERCICIO 3**")
+
+function ejercicio3 (n) {
+   let xvalue = n;
+while (xvalue >= 0){
+   console.log (xvalue);
+   xvalue -= 0.5;
+  }
+}
+ function numerosImpares(){
+   let index = 1
+    for(index =1; index <= 100; index = 1+2) {
+      console.log(index);
+   }
+ } numerosImpares();
+
--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,4 +1,4 @@
-console.log("**Ejercicio 1**")
+console.log("**EJERCICIO 1**")
 let fila =["Sofia", "David", "Juan"];
 console.log("Parte #1");
 console.log(fila);
@@ -10,9 +10,10 @@ console.log(fila)
 fila.splice(1,0, "Renata");
 fila.push("Elena");
 fila.shift();
-console.log("Parte #1");
+console.log("Parte #3");
+console.log(fila);
 
-console.log("** Ejercicio 2**")
+console.log("** EJERCICIO 2**")
 
 for(let i = 1; i <= 5; ++i ) {
    let resultado = "";
@@ -22,4 +23,19 @@ for(let i = 1; i <= 5; ++i ) {
    }console.log(resultado);
 }
 
- 
+console.log("***EJERCICIO 3**")
+
+function ejercicio3 (n) {
+   let xvalue = n;
+while (xvalue >= 0){
+   console.log (xvalue);
+   xvalue -= 0.5;
+  }
+}
+ function numerosImpares(){
+   let index = 1
+    for(index =1; index <= 100; index = 1+2) {
+      console.log(index);
+   }
+ } numerosImpares();
+
